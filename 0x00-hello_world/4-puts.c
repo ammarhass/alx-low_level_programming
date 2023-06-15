5aa53a754c076669a119e95484929977908caa87
Super User <ammar.hass94@gmail.com>
2023-06-15 04:14:13 -0700

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+/**
+ * main - function to print a string
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
+
