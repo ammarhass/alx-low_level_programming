dc7163008d45955722b32f1166f79a49f87a7714
Super User <ammar.hass94@gmail.com>
2023-06-22 04:55:38 -0700

    Task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include"main.h"
+
+/**
+ * mul - muliply two numbers
+ * @a: int num
+ * @b: int num
+ * Return: result of mul
+ */
+
+int mul(int a, int b)
+{
+	return ((long)(a * b));
+}
